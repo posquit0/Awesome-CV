5072e62a3bfee62b3f1d5e48d59dfded798d8db9
Ravi Joshi <ravi2008joshi@gmail.com>
2019-07-22 10:48:41 +0900

    added support for Google scholar and ORCiD icons

---
+++ examples/README.md
@@ -0,0 +1,14 @@
+Prerequisites
+    Make sure Font Awesome and Academicons are installed. 
+
+How to install the prerequisites mentioned above?
+  Go inside font directory. Double click on *.ttf file and install accordingly.
+
+Complie
+    $ xelatex cv.tex
+    $ biber cv
+
+    Or just run
+        sh compile.sh
+
+
---
+++ examples/compile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# compile cv.tex to produce cv.pdf
+# remove temporary files generated during compilation
+# Usage: sh compile.sh
+# Author: Ravi Joshi
+# Date: 16 June 2019
+# -------------------------------------------------
+
+echo "Running xelatex. Please wait..."
+xelatex cv.tex
+
+echo "Running biber. Please wait..."
+biber cv
+
+echo "Running xelatex. Please wait..."
+xelatex cv.tex
+
+echo "Removing temporary files. Please wait..."
+rm cv.aux cv.bbl cv.bcf cv.blg cv.log cv.out cv.run.xml
+
+echo "Finished."
