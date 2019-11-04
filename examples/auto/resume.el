490d9780aefe875c70904463252d76e4500c59a9
Nick Asendorf <nick.asendorf@gmail.com>
2019-11-03 20:44:30 -0600

    changing basic personal info and work formatting

---
+++ examples/auto/resume.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook "resume"
+ (lambda ()
+    (TeX-run-style-hooks
+     "latex2e"
+     "awesome-cv11"
+     "awesome-cv"
+     "11pt"
+     "a4paper"
+     "resume/summary"
+     "resume/education"
+     "resume/experience"
+     "resume/academia"
+     "resume/honors"
+     "resume/presentation"
+     "resume/committees"
+     "resume/extracurricular")))
+
